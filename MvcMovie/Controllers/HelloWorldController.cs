5c18bbe10b890ce7baf1fec18c3a8e4881026fef
baron <baron@vinova.sg>
2019-03-11 22:43:05 +0700

    View Controler

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,3 +1,4 @@
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMovie.Controllers
@@ -5,10 +6,27 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET
-        public IActionResult Index()
+        public string Index()
         {
-            return
-            View();
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+//        public string Welcome()
+//        {
+//            return "This is the Welcome action method...";
+//        }
+
+        // GET: /Hello/{name}
+        public string Hello(string name)
+        {
+            return "Hello, " + name + "!";
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
